4d4cff7d2269e789047c7ae96fe3c02736c9c352
Sigstus985 <siggeluren@gmail.com>
2024-12-23 16:10:54 +0100

    played around with fetching and added an image

--- src/api.js
+++ src/api.js
@@ -1,5 +1,14 @@
 export async function fetchData() {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto")
-    const data = await response.json()
-    console.log(data.name)
+	try {
+		for (let i = 0; i < 10; i++) {
+			const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 152)+1}`);
+			const data = await response.json();
+			console.log(data.name);
+		}
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+	} catch (error) {
+		console.error(error);
+	}
 }

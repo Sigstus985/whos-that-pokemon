802aad13ea8bdd72d91932c66121d7cca520d02c
Sigstus985 <siggeluren@gmail.com>
2024-12-28 22:01:31 +0100

    tablet responsiveness now works

---
+++ src/JS/playIntro.js
@@ -0,0 +1,9 @@
+/* export function playIntro() {
+	let audio = new Audio("intro.mp3");
+	audio.play();
+}
+
+document.getElementById("go").addEventListener("click", () => {
+	playIntro();
+});
+ */
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css"
-import { fetchData } from "./api";
+import { fetchData } from "./JS/api";
+//import { playIntro } from "./JS/playIntro";
 
 fetchData()

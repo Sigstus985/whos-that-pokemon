4375a71a24912d176f60968118cad765d48eb1b3
Sigstus985 <siggeluren@gmail.com>
2024-12-28 18:33:30 +0100

    fixed problem with folders

--- src/api.js
+++ src/api.js
@@ -1,21 +1,15 @@
 export async function fetchData() {
 	try {
-		for (let i = 0; i < 4; i++) {
-			//fetch data and save it as a json variable, log the name of the first 3 pokemon and render the 4th
-			if (i === 3){
-				let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1025)+1}`);
-				let data = await response.json();
-				document.getElementById("sprite").src = data.sprites.front_default
-			}
-			else {
-				let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1025)+1}`);
-				let data = await response.json();
-				console.log(data.name)
-			}
+		let randomPokemon = Math.floor(Math.random() * 1025) + 1;
+		console.log(randomPokemon);
+		let response = await fetch(
+			`https://pokeapi.co/api/v2/pokemon/${randomPokemon}`
+		);
+		let data = await response.json();
+		document.getElementById("sprite").src = data.sprites.front_default;
+		if (!response.ok) {
+			throw new Error("Could not fetch resource");
 		}
-        if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
 	} catch (error) {
 		console.error(error);
 	}

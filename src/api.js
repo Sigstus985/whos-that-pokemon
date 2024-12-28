32b54a8c44a0ff38bfbcf911840ade3a70e23486
Sigstus985 <siggeluren@gmail.com>
2024-12-28 14:15:34 +0100

    fiddled around with API and set up responsiveness framework for the CSS

--- src/api.js
+++ src/api.js
@@ -1,9 +1,17 @@
 export async function fetchData() {
 	try {
-		for (let i = 0; i < 10; i++) {
-			const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 152)+1}`);
-			const data = await response.json();
-			console.log(data.name);
+		for (let i = 0; i < 4; i++) {
+			//fetch data and save it as a json variable, log the name of the first 3 pokemon and render the 4th
+			if (i === 3){
+				let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1025)+1}`);
+				let data = await response.json();
+				document.getElementById("sprite").src = data.sprites.front_default
+			}
+			else {
+				let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 1025)+1}`);
+				let data = await response.json();
+				console.log(data.name)
+			}
 		}
         if(!response.ok){
             throw new Error("Could not fetch resource");
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import "./style.css"
 import { fetchData } from "./api";
 
-//fetchData()
+fetchData()

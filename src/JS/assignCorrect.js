4f5ea836a8f47a54d5b0aff77d64ec4417c6c6d6
Sigstus985 <siggeluren@gmail.com>
2024-12-29 13:35:35 +0100

    fixed assignation of is_correct key

--- src/JS/assignCorrect.js
+++ src/JS/assignCorrect.js
@@ -4,15 +4,14 @@ export function assignCorrect() {
 	fetchData();
 	const pokeArray = JSON.parse(localStorage.getItem("pokeArray"));
 	const correctNumber = Math.floor(Math.random() * 4);
+	console.log(correctNumber);
 	for (let i = 0; i < pokeArray.length; i++) {
-        const key = "is_correct";
-        if (i === correctNumber) {
-            pokeArray[i][key] = true;
-        }
-        else {
-            pokeArray[i][key] = false;
-        }
+		const key = "is_correct";
+		if (i === correctNumber) {
+			pokeArray[i][key] = true;
+		} else {
+			pokeArray[i][key] = false;
+		}
 	}
-    console.log(correctNumber);
-    console.log(pokeArray);
+    console.log(pokeArray)
 }

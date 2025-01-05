c1227c57b4f4340785d803aa60f78492a5fb6ff6
Sigstus985 <siggeluren@gmail.com>
2025-01-05 16:21:13 +0100

    added dom manip to show correct answer

--- src/JS/buttonPress.js
+++ src/JS/buttonPress.js
@@ -1,12 +1,11 @@
-document.querySelectorAll(".button").forEach(function (elem) {
-	elem.addEventListener("click", (event) => {
+document.querySelectorAll(".button").forEach(function (button) {
+	button.addEventListener("click", (event) => {
 		check(event.target.value);
 	});
 });
 
-
 export function check(buttonNum) {
-    const correctNumber = Number(localStorage.getItem("correctNum"));
+	const correctNumber = Number(localStorage.getItem("correctNum"));
 	//console.log(Number(document.getElementById().value))
 	if (correctNumber === Number(buttonNum)) {
 		console.log("correct!");
@@ -16,7 +15,20 @@ export function check(buttonNum) {
 	changeColor();
 }
 
+
 function changeColor() {
-    const correctNumber = Number(localStorage.getItem("correctNum"))
-    document.getElementById(`btn${(correctNumber.value + 1)}`).classList.add("correct")
+    const correctNumber = Number(localStorage.getItem("correctNum"));
+	buttons = document.querySelectorAll(".button");
+    const button1 = document.getElementById("btn1");
+    const button2 = document.getElementById("btn2");
+    const button3 = document.getElementById("btn3");
+    const button4 = document.getElementById("btn4");
+    const correctBtn = document.getElementById(`btn${correctNumber + 1}`);
+
+	button1.classList.add("incorrect");
+	button2.classList.add("incorrect");
+	button3.classList.add("incorrect");
+	button4.classList.add("incorrect");
+	correctBtn.classList.add("correct");
+	correctBtn.classList.remove("incorrect");
 }
